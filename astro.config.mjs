6bae715d8924ac54eba9fe74c650b520f17a0c33
Russell Holbrook <russell.holbrook@me.com>
2024-02-13 13:52:24 -0500

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://russtacular.github.io',
+    base: '/nycc-first-aid',  
+});
